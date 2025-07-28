72cc423472c0f7dbba88478f9e24dddae28254f4
mariyaalex215 <mariyaalex215@gmail.com>
2025-07-28 18:19:35 +0530

    q2 added

---
+++ q26.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class q26{
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] marks=new int[5];
+
+        
+
+
+
+
+
+        marks[0]=scan.nextInt();
+        marks[1]=scan.nextInt();
+        marks[2]=scan.nextInt();
+        marks[3]=scan.nextInt();
+        marks[4]=scan.nextInt();
+    }
+    
+
+
+}
